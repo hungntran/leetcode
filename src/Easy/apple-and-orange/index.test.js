adee67e135d862c756df84d7a5982d0615863949
Hung Tran <hungntran@outlook.com>
2022-08-14 00:16:43 +0700

    solved Easy problem

---
+++ src/Easy/apple-and-orange/index.test.js
@@ -0,0 +1,5 @@
+// import { countApplesAndOranges } from '.';
+
+// test('Sample case', () => {
+//   expect(countApplesAndOranges(7, 11, 5, 15, [-2, 2, 1], [5, -6])).toEqual([1, 1]);
+// });
--- src/Easy/kangaroo/index.test.js
+++ src/Easy/kangaroo/index.test.js
@@ -1,21 +1,21 @@
-import kangaroo from '.';
+// import kangaroo from '.';
 
-test('21, 6, 47, 3', () => {
-  expect(kangaroo(21, 6, 47, 3)).toBe('NO');
-});
+// test('21, 6, 47, 3', () => {
+//   expect(kangaroo(21, 6, 47, 3)).toBe('NO');
+// });
 
-test('28, 8, 96, 2', () => {
-  expect(kangaroo(21, 6, 47, 3)).toBe('NO');
-});
+// test('28, 8, 96, 2', () => {
+//   expect(kangaroo(21, 6, 47, 3)).toBe('NO');
+// });
 
-test('43, 2, 70, 2', () => {
-  expect(kangaroo(43, 2, 70, 2)).toBe('NO');
-});
+// test('43, 2, 70, 2', () => {
+//   expect(kangaroo(43, 2, 70, 2)).toBe('NO');
+// });
 
-test('240, 575, 9179, 9986', () => {
-  expect(kangaroo(240, 575, 9179, 9986)).toBe('NO');
-});
+// test('240, 575, 9179, 9986', () => {
+//   expect(kangaroo(240, 575, 9179, 9986)).toBe('NO');
+// });
 
-test('0, 3, 4, 2', () => {
-  expect(kangaroo(0, 3, 4, 2)).toBe('YES');
-});
+// test('0, 3, 4, 2', () => {
+//   expect(kangaroo(0, 3, 4, 2)).toBe('YES');
+// });
