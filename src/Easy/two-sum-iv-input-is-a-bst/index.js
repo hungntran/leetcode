c7b6a99d9c37b922c7e4a9e8f241a44cf73cfe92
Hung Tran <hungntran@outlook.com>
2022-10-09 20:42:41 +0700

    solve: easy problem

---
+++ src/Easy/two-sum-iv-input-is-a-bst/index.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {boolean}
+ */
+var findTarget = function (root, k) {
+  const set = new Set();
+
+  function dfs(root) {
+    if (root == null) return false;
+
+    if (set.has(k - root.val)) {
+      return true;
+    }
+
+    set.add(root.val);
+
+    return dfs(root.left) || dfs(root.right);
+  }
+
+  return dfs(root);
+};
