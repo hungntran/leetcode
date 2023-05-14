c0541c55f4787a882712b1b279c0170680583c9a
Hung Tran <hungntran@outlook.com>
2023-05-15 00:26:01 +0700

    solve: find-all-numbers-disappeared-in-an-array

---
+++ src/Easy/find-all-numbers-disappeared-in-an-array/index.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers = function (nums) {
+  const set = new Set(nums);
+  const res = [];
+
+  for (let i = 1; i <= nums.length; i++) {
+    if (!set.has(i)) {
+      res.push(i);
+    }
+  }
+
+  return res;
+};
