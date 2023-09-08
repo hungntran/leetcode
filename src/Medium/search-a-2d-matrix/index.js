0090fce4ae38019ef5ec6d0ac0511d8c740d0157
Hung Tran <hungntran@outlook.com>
2023-09-08 22:15:11 +0700

    search-a-2d-matrix

--- src/Medium/search-a-2d-matrix/index.js
+++ src/Medium/search-a-2d-matrix/index.js
@@ -4,5 +4,25 @@
  * @return {boolean}
  */
 var searchMatrix = function (matrix, target) {
-  return matrix.flatMap((row) => row).indexOf(target) !== -1;
+  const m = matrix.length;
+  const n = matrix[0].length;
+
+  let left = 0;
+  let right = m * n - 1;
+
+  while (left <= right) {
+    const mid = parseInt(left + (right - left) / 2);
+    const j = mid % n;
+    const i = parseInt(mid / n);
+
+    if (matrix[i][j] === target) {
+      return true;
+    } else if (matrix[i][j] > target) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return false;
 };
