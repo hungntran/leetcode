046d1ee85fb7265bbe191a4c81e6920e6327f856
Hung Tran <hungntran@outlook.com>
2023-09-24 22:04:42 +0700

    longest-common-subsequence

---
+++ src/Medium/longest-common-subsequence/index.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} text1
+ * @param {string} text2
+ * @return {number}
+ */
+var longestCommonSubsequence = function (text1, text2) {
+  const m = text1.length;
+  const n = text2.length;
+  const dp = Array(m + 1)
+    .fill(0)
+    .map(() => Array(n + 1).fill(0));
+
+  for (let i = m - 2; i >= 0; i--) {
+    for (let j = n - 2; j >= 0; j--) {
+      if (text1[i] === text2[j]) {
+        dp[i][j] = 1 + dp[i + 1][j + 1];
+      } else {
+        dp[i][j] = Math.max(dp[i + 1][j], dp[i][j + 1]);
+      }
+    }
+  }
+
+  return dp[0][0];
+};
