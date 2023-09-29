843eec3d2bf1020469b3268a6bf92b8169ed95a1
Hung Tran <hungntran@outlook.com>
2023-09-29 21:21:43 +0700

    monotonic-array

---
+++ src/Easy/monotonic-array/index.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var isMonotonic = function (nums) {
+  let isIncreasing = true;
+  let isDecreasing = true;
+
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] > nums[i + 1]) {
+      isIncreasing = false;
+    }
+
+    if (nums[i] < nums[i + 1]) {
+      isDecreasing = false;
+    }
+  }
+
+  return isDecreasing || isIncreasing;
+};
