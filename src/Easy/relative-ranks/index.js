ff6197bcc95052ddbe3fd4e2fd426041d2d95d8d
Hung Tran <hungntran@outlook.com>
2024-05-08 23:34:19 +0700

    update relative-ranks

--- src/Easy/relative-ranks/index.js
+++ src/Easy/relative-ranks/index.js
@@ -3,25 +3,19 @@
  * @return {string[]}
  */
 var findRelativeRanks = function (score) {
-  const sorted = [...score].sort((a, b) => b - a);
+  const sorted = [...score].sort((a, b) => b - a)
 
-  sorted.forEach((item, index) => {
-    const i = score.indexOf(item);
-    score[i] = helper(index);
-  });
+  const map = sorted.reduce((prev, cur, i) => {
+    prev[cur] = getRankName(i)
+    return prev
+  }, {})
 
-  return score;
-};
+  return score.map((e) => map[e])
+}
 
-const helper = (indx) => {
-  switch (indx) {
-    case 0:
-      return 'Gold Medal';
-    case 1:
-      return 'Silver Medal';
-    case 2:
-      return 'Bronze Medal';
-    default:
-      return String(indx + 1);
-  }
-};
+function getRankName(i) {
+  if (i === 0) return 'Gold Medal'
+  if (i === 1) return 'Silver Medal'
+  if (i === 2) return 'Bronze Medal'
+  return String(i + 1)
+}
