adef69884c8c506bcfe18369a82640520da040d1
Hung Tran <hungntran@outlook.com>
2024-10-06 02:39:01 +0700

    permutation-in-string

--- src/Medium/permutation-in-string/index.js
+++ src/Medium/permutation-in-string/index.js
@@ -4,21 +4,38 @@
  * @return {boolean}
  */
 var checkInclusion = function (s1, s2) {
-  const size = s1.length;
+	if (s1.length > s2.length) {
+		return false
+	}
 
-  const sortedS1 = s1.split('').sort().join('');
+	const size = s1.length
+	const arr1 = new Array(26).fill(0)
+	const arr2 = new Array(26).fill(0)
+	const A_CHARCODE = 'a'.charCodeAt()
 
-  for (let i = 0; i <= s2.length - size; i++) {
-    const sorted = s2
-      .slice(i, i + size)
-      .split('')
-      .sort()
-      .join('');
+	for (let i = 0; i < s1.length; i++) {
+		arr1[s1[i].charCodeAt(0) - A_CHARCODE] += 1
+		arr2[s2[i].charCodeAt(0) - A_CHARCODE] += 1
+	}
 
-    if (sortedS1 === sorted) {
-      return true;
-    }
-  }
+	for (let i = 0; i < s2.length - size; i++) {
+		if (matches(arr1, arr2)) {
+			return true
+		}
 
-  return false;
-};
+		arr2[s2[i + size].charCodeAt(0) - A_CHARCODE] += 1
+		arr2[s2[i].charCodeAt(0) - A_CHARCODE] -= 1
+	}
+
+	return matches(arr1, arr2)
+}
+
+function matches(arr1, arr2) {
+	for (let i = 0; i < 26; i++) {
+		if (arr1[i] !== arr2[i]) {
+			return false
+		}
+	}
+
+	return true
+}
