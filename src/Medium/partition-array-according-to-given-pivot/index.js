40181972adcadb63393d24005589aa94a29e6f9e
hungntran <hungntran@outlook.com>
2025-03-03 22:32:38 +0700

    partition-array-according-to-given-pivot

---
+++ src/Medium/partition-array-according-to-given-pivot/index.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} pivot
+ * @return {number[]}
+ */
+var pivotArray = function (nums, pivot) {
+    const left = []
+    const mid = []
+    const right = []
+
+    for (const num of nums) {
+        if (num === pivot) {
+            mid.push(num)
+        } else if (num < pivot) {
+            left.push(num)
+        } else {
+            right.push(num)
+        }
+    }
+
+    return [...left, ...mid, ...right]
+}
