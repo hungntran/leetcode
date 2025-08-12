a4dfe13d2fd392a118b138699c0561d6a83b57d0
hungntran <hungntran@outlook.com>
2025-08-13 01:17:29 +0700

    ways-to-express-an-integer-as-sum-of-powers

---
+++ src/Medium/ways-to-express-an-integer-as-sum-of-powers/index.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} n
+ * @param {number} x
+ * @return {number}
+ */
+var numberOfWays = function (n, x) {
+    const MOD = 1e9 + 7
+
+    const dp = new Array(n + 1).fill(0).map(() => Array(n + 1).fill(0))
+
+    dp[0][0] = 1
+
+    for (let i = 1; i <= n; i++) {
+        const val = Math.pow(i, x)
+
+        for (let j = 0; j <= n; j++) {
+            dp[i][j] = dp[i - 1][j]
+            if (j >= val) {
+                dp[i][j] = (dp[i][j] + dp[i - 1][j - val]) % MOD
+            }
+        }
+    }
+
+    return dp[n][n]
+}
